5e05408d9f7fa1a7e37f0d8f6d7a65d5ee7e2ad8
Subash <Subashtech14@gmail.com>
2023-11-14 06:47:05 +0530

    Comments

--- 3.3 Comments/Main.cpp
+++ 3.3 Comments/Main.cpp
@@ -1,5 +1,13 @@
-#include<iostream>
-int main(){
+// Used for Single Line Comments
+/*
+1 ->Block Comments -> Used to comment multiple lines
+2 -> Compiler ignores comments
+3 -> We Can't nest Block comments
+*/
+#include <iostream>
+int main()
+{
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
+
