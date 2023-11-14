839e905bc47a02ff0e3d49e5420dc2581fe73021
Subash <Subashtech14@gmail.com>
2023-11-14 09:13:19 +0530

    Numbering System

---
+++ 3.6 Numbering System/Main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main(){
+    int a = 10; //Decimal
+    int b = 012; //Octal
+    int c = 0x12; //Hexadecimal
+    int d = 0b1010; //Binary
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
+    std::cout << d << std::endl;
+    return 0;
+}
