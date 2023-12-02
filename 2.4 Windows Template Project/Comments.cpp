57ed33cd1c7ccd35242dba89684b5b0a67e8a214
Subash <Subashtech14@gmail.com>
2023-12-02 05:41:01 +0530

    Comments

---
+++ 2.4 Windows Template Project/Comments.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+    // One Line Comment
+    /*
+    Multi-line block comment
+    Another Line
+    Oh! And another one !
+    */
+    // Print out some text
+    std::cout << "Hellp World in C++20!" << std::endl;
+}
---
+++ 4.0 Operation On Data/4.6 Logical Operators/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main()
+{
+    std::cout << "Enter the number" << std::endl;
+    return 1;
+}
